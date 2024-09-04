006e47cbf875698f6197e18381c8202592855aeb
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-04 09:53:50 -0400

    Completed the exercise.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
+  const textArray = text.split("");
+  let reverse = '';
 
+  for (let i = textArray.length - 1; i >= 0; i--) {
+    reverse += text.at(i);
+  }
+
+  return reverse;
 };
 
 // Do not edit below this line
