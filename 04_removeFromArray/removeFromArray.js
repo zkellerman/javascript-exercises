396de9ff67f38eaf2144759add9959b128450125
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-04 13:48:41 -0400

    Completed the exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+  for (let i = 0; i < items.length; i++) {
+    let item = items[i];
+    for (let j = 0; j < array.length; j++) {
+      if (array.at(j) === item) {
+        array.splice(j, 1);
+        j--;
+      }
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
