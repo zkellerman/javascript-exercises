aaa3c2bc1bfc9a578a1955f21e64267080c5797b
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-04 13:59:16 -0400

    Completed the exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let startNum = num1;
+  let endNum = num2;
 
+  if (startNum < 0 || endNum < 0 || !Number.isInteger(startNum) || !Number.isInteger(endNum))
+    return 'ERROR';
+
+  if (startNum > endNum) {
+    startNum = num2;
+    endNum = num1;
+  }
+
+  let sum = 0;
+
+  for (let currentNum = startNum; currentNum <= endNum; currentNum++)
+    sum += currentNum;
+
+  return sum;
 };
 
 // Do not edit below this line
