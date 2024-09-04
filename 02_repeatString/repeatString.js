c6ccee7261866c44d696c39493c67ee62168d66b
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-04 09:39:38 -0400

    Completed the exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(text, timesRepeated) {
+  if (timesRepeated < 0)
+    return 'ERROR'
 
+  let string = '';
+
+  while (timesRepeated > 0) {
+    string += text;
+    timesRepeated--;
+  }
+
+  return string;
 };
 
 // Do not edit below this line
