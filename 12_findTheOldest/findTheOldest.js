55721bd6e9603ac5f0ba470806363c263301bdd8
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-12 14:42:58 -0400

    Completed the exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  let oldestPerson = {};
+  let oldestAge = 0;
 
+  arr.forEach(person => {
+    let deathYear = person.yearOfDeath === undefined ? new Date().getFullYear() : person.yearOfDeath;
+    let age = deathYear - person.yearOfBirth;
+    if (age > oldestAge) {
+      oldestPerson = person;
+      oldestAge = age;
+    }
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
