634866bfadf89018f22437ef81335ac69e17d2ed
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-12 11:34:40 -0400

    Completed the exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const filteredArr = string.toUpperCase().split('').filter((char) => {
+    return (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9')
+  });
+  let filteredString = filteredArr.join('');
 
+  const reversedArr = filteredString.split('').reverse();
+  let reversedString = reversedArr.join('');
+
+  return filteredString === reversedString;
 };
 
 // Do not edit below this line
