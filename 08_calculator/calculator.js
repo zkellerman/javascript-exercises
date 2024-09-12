e8cdd920f8c978627636cb731754904e5a27d2b4
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-12 09:35:49 -0400

    Completed the exercise.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, curr) => { return sum + curr }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prod, curr) => { return prod * curr });
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) return 1;
+
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
