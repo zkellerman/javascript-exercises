7a47477bdaf11d4d7405247b2b366e70252a9caa
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-12 11:56:26 -0400

    Completed the exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(sequence) {
+  sequence = parseInt(sequence);
+  if (sequence < 0) return 'OOPS';
+  if (sequence === 0) return 0;
 
+  const arr = [];
+
+  for (let i = 0; i < sequence; i++) {
+    if (arr.length < 2) 
+      arr.push(1);
+    else
+      arr.push(arr.at(-1) + arr.at(-2));
+  }
+
+  return arr.pop();
 };
 
 // Do not edit below this line
